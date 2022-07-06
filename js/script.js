63b7ae703b21afe9983898a8572ae5c74a0d4d6f
AlexBosio95 <alex.bosio1995@gmail.com>
2022-07-06 10:45:28 +0200

    correction

--- js/script.js
+++ js/script.js
@@ -1,39 +1,10 @@
-// const slides = [
-//     {
-//         image: 'img/01.jpg',
-//         title: 'Svezia',
-//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-//     },
-//     {
-//         image: 'img/02.jpg',
-//         title: 'Svizzera',
-//         text: 'Lorem ipsum.',
-//     },
-//     {
-//         image: 'img/03.jpg',
-//         title: 'Gran Bretagna',
-//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-//     },
-//     {
-//         image: 'img/04.jpg',
-//         title: 'Germania',
-//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-//     },
-//     {
-//         image: 'img/05.jpg',
-//         title: 'Paradise',
-//         text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-//     }
-// ];
-
-// console.log(slides[0].image);
 
 const app = new Vue({
     el: '#app',
     data: {
         clock: 0,
         isPlay: true,
-        index: 0,
+        indexActive: 0,
         slides: [
             {
                 image: 'img/01.jpg',
@@ -64,9 +35,9 @@ const app = new Vue({
     },
     methods: {
         nextSlide: function () {
-            this.index++
-            if (this.index == this.slides.length) {
-                this.index = 0;
+            this.indexActive++
+            if (this.indexActive == this.slides.length) {
+                this.indexActive = 0;
             }
 
             if (!this.isPlay) {
@@ -77,9 +48,9 @@ const app = new Vue({
         },
 
         prevSlide: function () {
-            this.index--;
-            if (this.index == - 1) {
-                this.index = this.slides.length - 1;
+            this.indexActive--;
+            if (this.indexActive == - 1) {
+                this.indexActive = this.slides.length - 1;
             }
 
             if (!this.isPlay) {
@@ -92,9 +63,9 @@ const app = new Vue({
         playClock: function () {
             if (this.isPlay) {
                 this.clock = setInterval(() => {
-                    this.index++
-                    if (this.index == this.slides.length) {
-                        this.index = 0;
+                    this.indexActive++
+                    if (this.indexActive == this.slides.length) {
+                        this.indexActive = 0;
                     }
                 }, 3000);
 
